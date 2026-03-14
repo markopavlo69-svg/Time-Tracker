c7d230866457fb30e1675a6cec784d301df1e0a8
Marko <markopavlo.69@gmail.com>
2026-03-14 14:19:48 +0100

    started working on function to create cards but i cant find out how to push it correctly to the array with one unique idea

--- time_tracker.js
+++ time_tracker.js
@@ -4,6 +4,13 @@ const addBtn = document.querySelector(".add-btn");
 const modal = document.querySelector(".modal")
 const submitBtn = document.querySelector(".submit-btn")
 const cancelBtn = document.querySelector(".cancel-btn")
+const cardContainer = document.querySelector(".card-container")
+const itemName = document.querySelector(".item-name")
+const itemCat = document.querySelector(".item.category")
+const itemDesc = document.querySelector(".item-desc")
+
+//Vars
+let cards = [];
 //Event Listener
 
 addBtn.addEventListener("click", showModal);
@@ -21,3 +28,17 @@ function showModal(){
 function hideModal(){
     modal.style.display = "none";
 }
+
+function  createCard (){
+    let id = 0
+
+ const newCard = {
+    id: id++,
+    name : itemName.value,
+    category : itemCat.value,
+    description : itemDesc.value
+ }
+ cards.push(newCard);
+ console.log(cards);
+
+};
